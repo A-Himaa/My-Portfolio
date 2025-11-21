592998ac0f8f136f6fb23bf33147397c5c6b93d8
A-Himaa <akilahimaja@hotmail.com>
2025-11-21 12:40:42 +0530

    Finalized NavBar with animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,36 @@
 import logo1 from "../assets/logo1.png";
 
 export default function Navbar() {
+
   return (
-    <nav className="w-full top-0 fixed bg-bgColor backdrop-blur-md z-50">
+    <nav className="w-full top-0 fixed bg-bgColor backdrop-blur-md z-50 shadowGlow shadowAccent py-2">
       <div className="flex items-center justify-between max-w-6xl mx-auto px-6 py-4 ">
 
         {/* Logo */}
         <div className="flex items-center gap-2">
-          <img
+          <a href="#"><img
             src={logo1}
-            className="h-15 w-auto"
+            className="h-12 w-auto"
             alt="logo" />
-          <span className="text-md font-semibold font-poppins text-secTextColor">Akila Himaja</span>
+          </a>
+          <span className="text-base font-semibold font-poppins text-secTextColor pt-1">Akila Himaja</span>
         </div>
 
+        {/* Navigation Links */}
         <ul className="hidden md:flex items-center text-textColor gap-12">
-          <li className="hover:text-secTextColor transition"><a href="#">About</a></li>
-          <li className="hover:text-secTextColor transition"><a href="#">Projects</a></li>
-          <li className="hover:text-secTextColor transition"><a href="#">Skills</a></li>
-          <li className="hover:text-secTextColor transition"><a href="#">Contact</a></li>
+          <li className="hover:text-accent transition duration-300"><a href="#">About</a></li>
+          <li className="hover:text-accent transition duration-300"><a href="#">Projects</a></li>
+          <li className="hover:text-accent transition duration-300"><a href="#">Skills</a></li>
+          <li className="hover:text-accent transition duration-300"><a href="#">Contact</a></li>
           <li>
             <a 
               href="#"
               target="_blank"
-              className="border border-2 border-textColor px-4 py-2 rounded-md font-semibold">
+              className="border border-2 border-textColor px-6 py-3 rounded-md font-semibold hover:text-accent buttonHover">
                 Resume
             </a>
           </li>
         </ul>
-
-        <div className="md:hidden text-textColor text-2xl cursor-pointer">☰</div>
 
 
       </div>
