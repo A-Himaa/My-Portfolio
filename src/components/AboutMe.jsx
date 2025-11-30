db4b938c0fec6cacd42abbab6c7a15d571a8ddb4
A-Himaa <akilahimaja@hotmail.com>
2025-11-30 17:53:10 +0530

    Minor changes in about page

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,16 +2,26 @@
 export default function About() {
     return(
         <section className="w-full bg-bgColor">
-            <div>
-                <div></div>
-                <div>
+            <div className="max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-2 mx-auto">
+                <div className="relative ">
+
+                </div>
+
+                {/*-----------Right Section------------*/}
+                <div className="space-y-2">
+                    <p>About Me</p>
+                    <p>Curious Developer, Tech Explorer, Problem Solver</p>
                     <p> I'm a passionate and detail-oriented software engineer with a strong interest 
             in building modern, efficient, and user-focused digital experiences. I enjoy 
             transforming ideas into functional and visually appealing applications using 
             clean code, creative design, and modern technologies.</p>
+                    <p> My journey in the tech world is driven by curiosity and continuous learning. 
+            Whether it's full-stack development, UI/UX, or solving complex backend logic, 
+            I love diving deep and exploring new ways to improve my craft.</p>
                 </div>
             </div>
             
         </section>
     )
 }
+
