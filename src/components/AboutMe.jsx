d4939abc552bb692a31b87fc7c530f56e80f55cb
A-Himaa <akilahimaja@hotmail.com>
2025-12-01 20:54:08 +0530

    Add animations to About component for enhanced user experience

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,29 @@
-import React from "react";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
     const [activeTab, setActiveTab] = useState("About Me");
+
+    // Animation
+    const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0, y: 20 },
+  show: { 
+    opacity: 1, 
+    y: 0, 
+    transition: { duration: 0.6, ease: "easeOut" } 
+  },
+};
+
 
     return(
         <section className="w-full bg-bgColor py-20 min-h-screen">
@@ -36,34 +57,61 @@ export default function About() {
 
                 {/*-----About Me----*/}
                 {activeTab === "About Me" && (
-                  <div className="space-y-6">
+                  <motion.div 
+                    className="space-y-6" 
+                    variants={container}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: false, amount: 0.25 }}
+                  >
                     <p className="font-mono text-sm md:text-base bg-accent/20 text-gray-100/90 px-2 py-1 w-1/2 md:w-1/4 tracking-wider rounded-sm">
                         ABOUT ME
                     </p>
 
-                    <h1 className="font-bold text-2xl md:text-5xl text-textColor tracking-wide pb-2">Professional Profile</h1>
+                    <motion.h1 
+                      className="font-bold text-2xl md:text-5xl text-textColor tracking-wide pb-2" 
+                      variants={item}
+                    >
+                      Professional Profile
+                    </motion.h1>
 
-                    <p className="text-base text-textColor tracking-wide">
+                    <motion.p 
+                      className="text-base text-textColor tracking-wide" 
+                      variants={item}
+                    >
                       I’m a dedicated full-stack web developer with a strong focus on building modern, efficient, and seamless digital experiences. I enjoy translating ideas into elegant, high-performance applications by combining clean code, intuitive UI/UX, and up-to-date technologies.
-                    </p>
+                    </motion.p>
 
-                    <p className="text-base text-textColor tracking-wide"> 
+                    <motion.p 
+                      variants={item}
+                      className="text-base text-textColor tracking-wide"> 
                       My journey in tech is fueled by curiosity and the desire to grow. From crafting responsive interfaces and designing scalable architectures to solving complex backend problems, I always strive to enhance both functionality and user experience. I’m constantly learning, experimenting, and pushing myself to create better, smarter solutions.
-                    </p>
-                  </div>
+                    </motion.p>
+                  </motion.div>
 
                   )}
 
                   {/*-----Background----*/}
                   {activeTab === "Background" && (
-                    <div className="space-y-6">
+                    <motion.div 
+                      className="space-y-6"
+                      variants={container}
+                      initial="hidden"
+                      whileInView="show"
+                      viewport={{ once: false, amount: 0.25 }}
+                    >
+
                       <p className="font-mono text-sm md:text-base bg-accent/20 text-gray-100/90 px-2 py-1 w-1/2 md:w-1/4 tracking-wider rounded-sm">
                           BACKGROUND
                       </p>
 
                       <div className="pb-2">
+
                         {/* ---Row 1--- */}
-                        <div className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2">
+                        <motion.div 
+                          className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2" 
+                          variants={item}
+                        >
                             <div className="bg-bgColor border border-accent/30 px-4 py-3 rounded-md">
                               <p className="text-sm text-mutedText">Name</p>
                               <p className="text-textColor font-medium">W Akila Himaja Tissera</p>
@@ -73,9 +121,13 @@ export default function About() {
                               <p className="text-sm text-mutedText">Location</p>
                               <p className="text-textColor font-medium">Negombo, Sri Lanka</p>
                             </div>
-                        </div>
+                        </motion.div>
+
                         {/* ---Row 2--- */}
-                        <div className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2">
+                        <motion.div 
+                          className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2" 
+                          variants={item}
+                        >
                             <div className="bg-bgColor border border-accent/30 px-4 py-3 rounded-md">
                               <p className="text-sm text-mutedText">Email</p>
                               <p className="text-textColor font-medium">akilatissera400@gmail.com</p>
@@ -85,17 +137,25 @@ export default function About() {
                               <p className="text-sm text-mutedText">Contact</p>
                               <p className="text-textColor font-medium">+94 767369080</p>
                             </div>
-                        </div>
+                        </motion.div>
+
                         {/* ---Row 3--- */}
-                        <div className="mb-2">
+                        <motion.div 
+                          className="mb-2" 
+                          variants={item}
+                        >
                             <div className="bg-bgColor border border-accent/30 px-4 py-3 rounded-md">
                               <p className="text-sm text-mutedText">Acadamic Background</p>
                               <p className="text-textColor font-medium">BSc (Hons) in Information Technology (Undergraduate) - SLIIT</p>
                               <p className="text-textColor font-medium">Ave Maria Convent (School)</p>
                             </div>
-                        </div>
+                        </motion.div>
+
                         {/* ---Row 4--- */}
-                        <div className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2">
+                        <motion.div 
+                          className="flex grid grid-cols-1 md:grid-cols-2 gap-3 mb-2" 
+                          variants={item}
+                        >
                             <div className="bg-bgColor border border-accent/30 px-4 py-3 rounded-md">
                               <p className="text-sm text-mutedText">Current Role</p>
                               <p className="text-textColor font-medium">Intern Full-Stack Developer</p>
@@ -105,30 +165,47 @@ export default function About() {
                               <p className="text-sm text-mutedText">Experiance</p>
                               <p className="text-textColor font-medium">Intern SE at Rangiri Holdings</p>
                             </div>
-                        </div>
+                        </motion.div>
+
                       </div>
-                    </div>
+                    </motion.div>
                   )}
 
                   {/* Aspiration */}
                   {activeTab === "Aspiration" && (
-                    <div className="space-y-6">
+                    <motion.div 
+                      className="space-y-6"
+                      variants={container}
+                      initial="hidden"
+                      whileInView="show"
+                      viewport={{ once: false, amount: 0.25 }}
+                    >
+
                       <p className="font-mono text-sm md:text-base bg-accent/20 text-gray-100/90 px-2 py-1 w-1/2 md:w-1/4 tracking-wider rounded-sm">
                           ASPIRATION
                       </p>
-                      <p className="text-base text-textColor tracking-wide">
+
+                      <motion.p 
+                        className="text-base text-textColor tracking-wide" 
+                        variants={item}
+                      >
                         I aspire to grow into a highly skilled full-stack developer focused on building 
                         scalable, user-centric digital solutions. My goal is to deepen my expertise in 
                         modern web technologies, system architecture, and cloud-based development while 
                         contributing to impactful real-world projects. 
-                      </p>
-                      <p className="text-base text-textColor tracking-wide">
+                      </motion.p>
+
+                      <motion.p 
+                        className="text-base text-textColor tracking-wide" 
+                        variants={item}
+                      > 
                         I aim to work in a dynamic engineering environment where I can solve meaningful 
                         problems, collaborate with talented teams, and continuously refine my craft. In 
                         the long run, I hope to lead innovative projects that blend creativity, technology, 
                         and thoughtful design to create experiences that truly make a difference.
-                      </p>
-                    </div>
+                      </motion.p>
+                      
+                    </motion.div>
                   )}
                 </div>
 
