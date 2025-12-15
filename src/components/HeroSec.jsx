db2d784058611a7ffb2db27f017c96524f2ead3a
A-Himaa <akilahimaja@hotmail.com>
2025-12-15 16:53:06 +0530

    Refactor Hero section layout; update dark overlay styles and enhance content structure

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -9,9 +9,6 @@ export default function HeroSec() {
         }
     }, []);
 
-
-
-
     return (
         <section>
             {/* Video Background */}
@@ -25,8 +22,26 @@ export default function HeroSec() {
                 playsInline/>
 
                 {/* Dark Overlay */}
-            <div className="absolute inset-0 bg-gradient-to-b from-black/30 to-black/40" />
-            
+            <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black/50" />
+
+            {/* Overlay Content */}
+            <div className="relative items-center justify-center flex flex-col mx-auto max-w-3xl min-h-screen">
+                <h3>Software Engineer</h3>
+                <h1>HIMAJA</h1>
+                <h1>TISSERA</h1>
+                <h3>Architecting the future web</h3>
+                <p>
+                    I build scalable and efficient web applications with a focus on user-centric solutions. 
+                    Passionate about tackling complex challenges and delivering innovative digital experiences 
+                    that combine performance, usability, and creativity.
+                </p>
+                <div>
+                    <button>Contact Me</button>
+                    <button>View My Work</button>
+                </div>
+            </div>
+
+        
         </section>
     )
 }
